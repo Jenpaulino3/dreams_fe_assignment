1a3e3d61e8056fb2f38d222521b7491d9fbd0332
JenLovesCode <jenpaulino3@gmail.com>
2019-07-21 14:23:55 -0400

    initial commit with HomePage and VideoPlayer components

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="video-player">
+        <div id="player">
+            <img class="video" :src="require('../assets/images/video-image.png')">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+    .video-player {
+        #player {
+            img {
+                width: 100%;
+            }
+        }
+    }
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="homepage">
+        <VideoPlayer />
+    </div>
+</template>
+<script>
+import VideoPlayer from '@/components/VideoPlayer'
+export default {
+    components: {
+        VideoPlayer
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .homepage {
+        #player {
+            img {
+                width: 100%;
+            }
+        }
+    }
+</style>
