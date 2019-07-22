c44eeb1f435d42cb6dd46140707fb4de9be2055b
JenLovesCode <jenpaulino3@gmail.com>
2019-07-21 20:57:00 -0400

    moved marker to its own component and set cursor to pointer

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -8,24 +8,9 @@
                 <div class="progress-bar">
                     <div class="playhead"></div>
                 </div>
-                <div class="marker"
-                        v-for="marker in videoInfo.markers" 
-                        :key="marker.id"
-                        :style="markerPosition(marker.timecode)"
-                        @mouseover="onMarkerEvent(true)"
-                        @mouseleave="onMarkerEvent(false)">
-                        <svg 
-                            xmlns="http://www.w3.org/2000/svg" 
-                            width="24" 
-                            height="24" 
-                            viewBox="0 0 24 24">
-                                <path d="M0 0h24v24H0z" fill="none"/>
-                                <path 
-                                    d="M17.63 5.84C17.27 5.33 16.67 5 16 5L5 5.01C3.9 5.01 3 5.9 3 7v10c0 1.1.9 1.99 2 1.99L16 19c.67 0 1.27-.33 1.63-.84L22 12l-4.37-6.16z" 
-                                    :fill="markerIsHovered ? '#00A3FF' : 
-                                    '#fff' " />
-                        </svg>
-                    </div>
+                <VideoPlayerMarker v-for="marker in videoInfo.markers" 
+                    :key="marker.id"
+                    :style="markerPosition(marker.timecode)" />
                 <div class="player-controls">
                     <div class="control" v-for="control in playerControls" :key="control">
                     </div>
@@ -35,14 +20,17 @@
     </div>
 </template>
 <script>
+import VideoPlayerMarker from '@/components/VideoPlayerMarker'
 export default {
+    components: {
+        VideoPlayerMarker
+    },
     data () {
         return {
             container: null,
             videoImage: require('../assets/images/video-image.png'),
             setPlayerUIDisplay: null,
             markerSVG: require('../assets/marker-24px.svg'),
-            markerIsHovered: false,
             videoInfo: {
                 duration: 547,
                 markers: [
@@ -80,10 +68,6 @@ export default {
             let calc = (100/this.videoInfo.duration) * timecode + 'px'
             let position = { left: calc }
             return position
-        },
-        onMarkerEvent (condition) {
-            condition === true ? this.markerIsHovered = true 
-                : this.markerIsHovered = false
         }
     }
 }
@@ -97,7 +81,6 @@ export default {
             .player-ui {
                 display: block;
                 .progress-bar {
-                    display: flex;
                     position: relative;
                     height: 5px;
                     top: -45px;
@@ -107,13 +90,6 @@ export default {
                         height: 5px;
                         background-color: #fff;
                         width: 40px;
-                    }
-                }
-                .marker {
-                    position: absolute;
-                    margin-top: -75px;
-                    svg {
-                        transform: rotate(90deg);
                     }
                 }
             }
---
+++ src/components/VideoPlayerMarker.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div class="marker"
+            @mouseover="onMarkerEvent(true)"
+            @mouseleave="onMarkerEvent(false)">
+            <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="24" 
+                height="24" 
+                viewBox="0 0 24 24">
+                    <path d="M0 0h24v24H0z" fill="none"/>
+                    <path 
+                        d="M17.63 5.84C17.27 5.33 16.67 5 16 5L5 5.01C3.9 5.01 3 5.9 3 7v10c0 1.1.9 1.99 2 1.99L16 19c.67 0 1.27-.33 1.63-.84L22 12l-4.37-6.16z" 
+                        :fill="markerIsHovered ? '#00A3FF' : 
+                        '#fff' " />
+            </svg>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            markerIsHovered: false
+        }
+    },
+    methods: {
+        onMarkerEvent (condition) {
+            condition === true ? this.markerIsHovered = true 
+                : this.markerIsHovered = false
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    div {
+        display: flex;
+        position: relative;
+        .marker {
+            position: absolute;
+            margin-top: -75px;
+            cursor: pointer;
+            svg {
+                transform: rotate(90deg);
+            }
+        }
+    }
+</style>
